ce03b0c16b1b6e38539b557bfd83f95ba8496f93
aguyfromdenmark <ankrweb@gmail.com>
2017-05-16 09:57:26 +0200

    Added models

---
+++ Model/OrderModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class OrderModel
+    {
+        public string Id { get; set; }
+        public DateTime DatePlaces { get; set; }
+        public List<OrderLineModel> Lines { get; set; }
+        public int TotalPrice { get; set; }
+
+        public OrderModel()
+        {
+            Lines = new List<OrderLineModel>();    
+        }
+    }
+}
