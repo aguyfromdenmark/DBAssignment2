62bcf644c60b2ff7b8e4240389fa60557e26140d
aguyfromdenmark <ankrweb@gmail.com>
2017-05-16 09:45:46 +0200

    Add project files.

---
+++ DBAssignment2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DBAssignment2.MvcApplication" Language="C#" %>
---
+++ DBAssignment2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DBAssignment2.Startup))]
+namespace DBAssignment2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
