9cc12a0d93bec7c2ea6525f450a646e77f13c5e4
aguyfromdenmark <ankrweb@gmail.com>
2017-05-16 11:42:56 +0200

    Added Customer BLL with methods

---
+++ BLL/CustomerBLL.cs
@@ -0,0 +1,46 @@
+﻿using DAL;
+using Model;
+using System.Collections.Generic;
+
+namespace BLL
+{
+    public class CustomerBLL
+    {
+        private CustomerDAL _customerDAL;
+
+        public CustomerBLL()
+        {
+            _customerDAL = new CustomerDAL();
+        }
+
+        public CustomerModel GetCustomerById(string id)
+        {
+            return _customerDAL.GetSingle(id);
+        }
+
+        public CustomerModel GetCustomerByPhone(string phoneNumber)
+        {
+            return _customerDAL.GetByPhone(phoneNumber);
+        }
+
+        public List<CustomerModel> GetAllCustomers()
+        {
+            return _customerDAL.GetAll();
+        }
+
+        public void CreateCustomer(CustomerModel customer)
+        {
+            _customerDAL.Create(customer);
+        }
+
+        public void DeleteCustomer(string customerId)
+        {
+            _customerDAL.Delete(customerId);
+        }
+
+        public void UpdateCustomer(CustomerModel customer)
+        {
+            _customerDAL.Update(customer);
+        }
+    }
+}
