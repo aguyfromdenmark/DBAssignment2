f9415aaf02612ef332eb0425b04eafc98ec743e2
aguyfromdenmark <ankrweb@gmail.com>
2017-05-16 10:23:07 +0200

    Added database interface

--- DAL/CustomerDAL.cs
+++ DAL/CustomerDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,36 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    public class CustomerDAL
+    public class CustomerDAL : IDatabase<CustomerModel>
     {
+        public void Create(CustomerModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string itemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CustomerModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerModel GetSingle(string itemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(CustomerModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerModel GetByPhone(string phoneNuber)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ DAL/IDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface IDatabase<T>
+    {
+        T GetSingle(string itemId);
+        List<T> GetAll();
+        void Create(T item);
+        void Update(T item);
+        void Delete(string itemId);
+    }
+}
